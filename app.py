5a4c5f11df625a7f5d4597cd86c3bdb9ffe623cd
vinayak_0733 <vinayakumar464@gmail.com>
2025-11-29 14:28:36 +0530

    nltk download

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-
+nltk.download("punkt_tab")
+nltk.download('stopwords')
 ps = PorterStemmer()
 
 def text_transform(text):
